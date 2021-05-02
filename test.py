ab9d116fc3f8445f77a75b060c3fbf8f4b66a103
Amr Marey <romaniga1325@gmail.com>
2021-05-02 14:09:50 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,45 @@
+class Employee:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+#inhertance
+class HourlyEmployee(Employee):
+    def __init__(self, id, name, hours_worked, hour_rate):
+        super().__init__(id, name)
+        self.hours_worked = hours_worked
+        self.hour_rate = hour_rate
+
+        def calculate_payroll(self):
+            return self.hours_worked * self.hour_rate
+
+#composotions
+class Address:
+    def __init__(self, street, city, state, zipcode, street2=''):
+        self.street = street
+        self.street2 = street2
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+
+    def __str__(self):
+        lines = [self.street]
+        if self.street2:
+            lines.append(self.street2)
+        lines.append(f'{self.city}, {self.state} {self.zipcode}')
+        return '\n'.join(lines)
+
+class PayrollSystem:
+    def calculate_payroll(self, employees):
+        print('Calculating Payroll')
+        print('===================')
+        for employee in employees:
+            print(f'Payroll for: {employee.id} - {employee.name}')
+            print(f'- Check amount: {employee.calculate_payroll()}')
+            if employee.address:
+                print('- Sent to:')
+                print(employee.address)
+            print('')
+
+
+
